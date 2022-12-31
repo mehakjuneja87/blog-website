cfae75c9e2794224f848d7bc46418a80ecb96083
Mehak <mehakjuneja87@gmail.com>
2022-12-31 16:32:10 +0530

    setup blog website

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React, {memo} from 'react'
+
+function Footer() {
+  return (
+    <div className="card-footer text-muted text-center font-weight-bolder p-5 m-2">
+      &copy; Mehak juneja
+    </div>
+  )
+}
+export default memo(Footer)
